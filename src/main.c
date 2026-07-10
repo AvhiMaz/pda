9cca0235cf0143cf54f6915377a9186e1905eb23
AvhiMaz <avhimazumder5@outlook.com>
2026-07-10 12:10:53 +0530

    wip: decode point bytes and extract sign bit
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/main.c
+++ src/main.c
@@ -1,4 +1,20 @@
+#include <stdint.h>
 #include <stdio.h>
+
+int pda_is_on_curve(const uint8_t p[32]) {
+    if (!p) {
+        return -1;
+    }
+
+    uint8_t be[32];
+    for (int i = 0; i < 32; i++) {
+        be[i] = p[31 - i];
+    }
+    int sign = be[0] >> 7;
+    be[0] &= 0x7F;
+
+    return 0;
+}
 
 int main(void) {
     printf("hello world\n");
